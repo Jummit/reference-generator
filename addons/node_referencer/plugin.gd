37dba929c61769cf3c972cd4556633c4cd738a2e
Jummit <jummit@web.de>
2021-06-04 07:51:44 +0200

    fix script getter

--- addons/node_referencer/plugin.gd
+++ addons/node_referencer/plugin.gd
@@ -21,20 +21,26 @@ func _exit_tree() -> void:
 
 
 func _on_ToolMenu_generate_class_reference_pressed(_ud) -> void:
-	var class_script := get_editor_interface().get_script_editor().get_current_script()
-	OS.clipboard = "const %s = preload(\"%s\")" % [snake_to_pascal_case(class_script.resource_path.get_file().get_basename()), class_script.resource_path]
+	var class_script := get_editor_interface().get_script_editor().\
+			get_current_script()
+	OS.clipboard = "const %s = preload(\"%s\")" % [snake_to_pascal_case(
+			class_script.resource_path.get_file().get_basename()),
+			class_script.resource_path]
 	print("Class reference code copied to clipboard")
 
 
 func _on_ToolMenu_generate_node_reference_pressed(_ud) -> void:
-	var selected_nodes := get_editor_interface().get_selection().get_selected_nodes()
-	var current_script := get_editor_interface().get_script_editor().get_current_script()
-	var relative_to_node := search_node_that_uses(get_editor_interface().get_edited_scene_root(), current_script)
+	var selected_nodes := get_editor_interface().get_selection().\
+			get_selected_nodes()
+	var current_script := get_editor_interface().get_script_editor().\
+			get_current_script()
+	var relative_to_node := search_node_that_uses(
+			get_editor_interface().get_edited_scene_root(), current_script)
 	
 	var reference_code := ""
 	for selected_node in selected_nodes:
-		selected_node = selected_node as Node
-		reference_code += get_node_reference(selected_node, relative_to_node.get_path_to(selected_node)) + "\n"
+		reference_code += get_node_reference(selected_node,
+				relative_to_node.get_path_to(selected_node)) + "\n"
 	
 	OS.clipboard = reference_code
 	print("Node reference code copied to clipboard")
@@ -63,10 +69,10 @@ static func get_node_name(string : String) -> String:
 
 
 static func search_node_that_uses(root : Node, script : Script) -> Node:
-	if root.script == script:
+	if root.get_script() == script:
 		return root
 	for node in root.get_children():
-		if node.script == script:
+		if node.get_script() == script:
 			return node
 		var found := search_node_that_uses(node, script)
 		if found.name != "":
